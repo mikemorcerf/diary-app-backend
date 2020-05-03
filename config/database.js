fe58e637193f57a254f39b3c7cbe909e6139c33b
Michael Moura <mikemorcerf@gmail.com>
2020-05-03 18:02:35 -0400

    change database host to connect to heroku

--- config/database.js
+++ config/database.js
@@ -2,7 +2,7 @@ module.exports = {
   username: process.env.ENV_DB_USER,
   password: process.env.ENV_DB_PASS,
   database: process.env.ENV_DB_NAME,
-  host: process.env.ENV_DB_HOST,
+  host: process.env.DATABASE_URL,
   port: process.env.ENV_DB_PORT,
   dialect: process.env.ENV_DB_DIALECT,
 }
