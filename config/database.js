3ba500ab193b7db9a1b12a4092039e151a143039
Michael Moura <mikemorcerf@gmail.com>
2020-05-03 20:01:29 -0400

    heroku db

--- config/database.js
+++ config/database.js
@@ -3,7 +3,7 @@ module.exports = {
   password: process.env.ENV_DB_PASS,
   database: process.env.ENV_DB_NAME,
   host: process.env.DATABASE_URL,
-  port: process.env.ENV_DB_PORT,
-  dialect: process.env.ENV_DB_DIALECT,
-  protocol: process.env.ENV_DB_DIALECT,
+  dialect: 'postgres',
+  protocol: 'postgres',
+  port: process.env.ENV_DB_PORT
 }
