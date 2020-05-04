e8a4f62dd90ff41b6d7757fe14d9042270a55a6e
Michael Moura <mikemorcerf@gmail.com>
2020-05-04 16:44:17 -0400

    update heroku db credentials

--- config/database.js
+++ config/database.js
@@ -2,9 +2,10 @@ module.exports = {
   username: process.env.ENV_DB_USER,
   password: process.env.ENV_DB_PASS,
   database: process.env.ENV_DB_NAME,
-  url: process.env.DATABASE_URL,
+  storage: process.env.DATABASE_URL,
   host: process.env.ENV_DB_HOST,
+  port: process.env.ENV_DB_PORT,
   dialect: 'postgres',
   protocol: 'postgres',
-  port: process.env.ENV_DB_PORT
+  ssl: true
 }
