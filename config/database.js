6782617eba78e7050660da7c2c743084c9df082e
Michael Moura <mikemorcerf@gmail.com>
2020-05-04 17:03:11 -0400

    added ssl connection to db

--- config/database.js
+++ config/database.js
@@ -6,6 +6,9 @@ module.exports = {
   host: process.env.ENV_DB_HOST,
   port: process.env.ENV_DB_PORT,
   dialect: 'postgres',
+  dialectOptions: {
+    ssl: true
+  },
   protocol: 'postgres',
   ssl: true
 }
