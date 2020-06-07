5a9407f156e99df88c8e5c2a99822f393aa292f9
Michael Moura <mikemorcerf@gmail.com>
2020-06-07 18:26:02 -0400

    update db config

--- config/database.js
+++ config/database.js
@@ -7,14 +7,21 @@ module.exports = {
   database: process.env.ENV_DB_NAME_DEV,
   // storage: process.env.DATABASE_URL,
   // url: process.env.DATABASE_URL,
-  // host: 'localhost',
-  // port: '5434',
+  host: 'localhost',
+  port: '5434',
   dialect: 'postgres',
   type: 'postgres',
   protocol: 'postgres',
   // migrationsRun: true
   },
   "production": {
-    "use_env_variable": process.env.DATABASE_URL
+    host: process.env.DATABASE_HOST,
+    database: process.env.DATABASE_NAME,
+    username: process.env.DATABASE_USER,
+    password: process.env.DATABASE_PASSWORD,
+    port: process.env.DATABASE_PORT,
+    URI: process.env.
+    DATABASE_URI,
+    // use_env_variable: process.env.DATABASE_URL
   }
 }
